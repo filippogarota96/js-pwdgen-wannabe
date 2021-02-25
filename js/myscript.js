69ac326a4039374d55d0987665131c518c15454f
Filippo Garota <filogarota101@gmail.com>
2021-02-25 15:49:23 +0100

    Aggiunto generatore di password

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('inserisci il tuo cognome');
+var colore = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML = nome + cognome + colore + 27;
